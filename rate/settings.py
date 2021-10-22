1a91b0f7d404842de5439cce6db5ecbc3aba1d22
1offmark <official1offmark@gmail.com>
2021-10-22 22:48:20 +0300

    updated installed apps in settings.py & installed django-bootstrap4

--- rate/settings.py
+++ rate/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rateapp.apps.RateappConfig',
+    'pyuploadcare.dj',
+    'bootstrap4',
+    'tinymce',
 ]
 
 MIDDLEWARE = [
